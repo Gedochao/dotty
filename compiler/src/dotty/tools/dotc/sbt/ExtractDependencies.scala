9d2675455a112361e21559127023f0d5a8ad4c28
Jan Chyb <48855024+jchyb@users.noreply.github.com>
2026-05-04 10:59:12 +0200

    Support zinc invalidation for type arguments in macro calls  (#23900)
    Fixes #23852 
    Closes #19426 
    
    Around a year ago a new `DependencyContext` value was added to the zinc
    api: `DependencyByMacroExpansion`.
    It causes the recorded dependency to trigger recompilation if there is
    any API change in the recorded type, not just if the feudal/method
    member that was explicitly referenced was changed. So now, for every
    inline call:
    `macroCall[T1, T2, ...]`, in file `call.scala`, if anything changes in a
    TN definition, `call.scala` is recompiled.
    
    Since `DependencyByMacroExpansion` was added later to the API,
    referencing it in older versions (e.g. any sbt < `1.10.0`), would cause
    crashes. to avoid that, we use reflection to check if that field exists.
    
    These changes do not seem to affect #23783, likely the way annotations
    are handled needs to be changed in zinc itself, as the APIInfo phase
    does successfully record the change in the annotation argument.
    
    Related discussion: https://github.com/sbt/zinc/pull/1316 (a PR adding
    the functionality to zinc and scala-2 plugin).
    
    ---------
    
    Co-authored-by: Jamie Thompson <bishbashboshjt@gmail.com>

--- compiler/src/dotty/tools/dotc/sbt/ExtractDependencies.scala
+++ compiler/src/dotty/tools/dotc/sbt/ExtractDependencies.scala
@@ -125,6 +125,9 @@ private class ExtractDependenciesCollector(rec: DependencyRecorder) extends Abst
   override def traverse(tree: Tree)(using Context): Unit =
     try
       recordTree(tree)
+
+      recordInlineCallArgs(tree)
+        
       tree match
         case tree: Inlined if !tree.inlinedFromOuterScope =>
           // The inlined call is normally ignored by TreeTraverser but we need to
@@ -182,6 +185,26 @@ trait AbstractExtractDependenciesCollector(rec: DependencyRecorder) extends tpd.
         rec.addClassDependency(parent.tpe.classSymbol, depContext)
     }
 
+  // Only reference DependencyByMacroExpansion if it an be found on the classpath,
+  // as it was added later to the zinc.apiinfo DependencyContext enum
+  // e.g. pre 1.10.x sbt would throw java.lang.NoSuchFieldError errors here
+  lazy val allowsDependencyByMacroExpansion =
+    classOf[DependencyContext].getFields().exists(_.getName() == "DependencyByMacroExpansion")
+
+  private def addMacroDependency(trees: List[Tree])(using Context): Unit =
+    if (allowsDependencyByMacroExpansion) {
+      val traverser = new TypeDependencyTraverser {
+        def addDependency(symbol: Symbol) =
+          if (!ignoreDependency(symbol)) {
+            val enclOrModuleClass = if (symbol.is(ModuleVal)) symbol.moduleClass else symbol.enclosingClass
+            assert(enclOrModuleClass.isClass, s"$enclOrModuleClass, $symbol")
+
+            rec.addClassDependency(enclOrModuleClass, DependencyByMacroExpansion)
+          }
+      }
+      trees.foreach(tree => traverser.traverse(tree.tpe))
+    }
+
   private def depContextOf(cls: Symbol)(using Context): DependencyContext =
     if cls.isLocal then LocalDependencyByInheritance
     else DependencyByInheritance
@@ -197,6 +220,12 @@ trait AbstractExtractDependenciesCollector(rec: DependencyRecorder) extends tpd.
     catch case ex: StaleSymbol =>
       // can happen for constructor proxies. Test case is pos-macros/i13532.
       true
+
+  protected def recordInlineCallArgs(tree: Tree)(using Context) =
+    tree match
+      case TypeApply(fun, args) if fun.symbol.is(Inline) =>
+        addMacroDependency(args)
+      case _ =>
 
   protected def recordTree(tree: Tree)(using Context): Unit =
     tree match
